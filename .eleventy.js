5ca6a264916ec7d410b33f0c0086a8aa8d9d9a6e
Elijah Walker <walke861@msu.edu>
2025-02-08 22:01:16 -0500

    added shortcode to avoid repetition

--- .eleventy.js
+++ .eleventy.js
@@ -3,4 +3,12 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+
+  eleventyConfig.addShortcode("planets", function(planet, name, description) {
+    return `<div class="card ${planet}">
+    <div class="circle"></div>
+    <h3>${name}</h3>
+    <p>${description}</p>
+</div>`;
+  });
 };
